580301bef4ceb11fcd9ded97f9feb5818650026a
xiaoxindada <2245062854@qq.com>
2021-03-25 13:54:29 +0800

    SGSI build: Build in the form of script passing parameters
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- make/romtype.sh
+++ make/romtype.sh
@@ -11,7 +11,7 @@ if [ $os_type = "Pixel" ];then
   echo "检测当前为pixel原生系统"
   #echo "正在完善特性"
   #./add_build.sh
-  ./add_etc_vintf_patch/pixel/add_vintf.sh
+  #./add_etc_vintf_patch/pixel/add_vintf.sh
   # rom修补处理
   ./rom_make_patch/pixel/make.sh
   echo "正在精简推广"
