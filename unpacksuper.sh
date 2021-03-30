166ff9859369362eefba133624815ef3257503be
xiaoxindada <2245062854@qq.com>
2021-03-30 22:28:49 +0800

    unpacksuper.sh: Super.img get image size before unpacking
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- unpacksuper.sh
+++ unpacksuper.sh
@@ -6,22 +6,27 @@ source ./bin.sh
 
 rm -rf ./super
 mkdir ./super
+
+super_size=$(du -sh `find -type f -name 'super.img'` | awk '{print $1}' | bc -q | sed 's/$/&G/')
+
 file $(find -type f -name 'super.img') > ./file.txt
 echo "识别到$(find ./ -type f -name 'super.img')"
 
 if [ $(grep -o 'sparse' ./file.txt) ];then
- echo "当前super.img转换为rimg中......"
- $bin/simg2img $(find ./ -type f -name 'super.img') ./superr.img
- echo "转换完成"
- echo "解压super.img中....."
- $bin/lpunpack ./superr.img ./super
- rm -rf ./superr.img
- chmod 777 -R ./super
- echo "解压完成"
+  echo "当前super.img转换为rimg中..."
+  $bin/simg2img $(find ./ -type f -name 'super.img') ./superr.img
+  echo "转换完成"
+  echo "当前super分区大小为: $super_size"
+  echo "解压super.img中..."
+  $bin/lpunpack ./superr.img ./super
+  rm -rf ./superr.img
+  chmod 777 -R ./super
+  echo "解压完成"
 fi
 
 if [ $(grep -o 'data' ./file.txt) ];then
-  echo "解压super.img中....."
+  echo "当前super分区大小为: $super_size"
+  echo "解压super.img中..."
   $bin/lpunpack $(find ./ -type f -name 'super.img') ./super
   #rm -rf ./super.img
   chmod 777 -R ./super
