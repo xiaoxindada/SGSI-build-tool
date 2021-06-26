3e085b011e95aa1be921e70e382722d480cab85f
xiaoxindada <2245062854@qq.com>
2021-06-27 06:44:33 +0800

    boot_tools: Update aik_tools > from https://github.com/osm0sis/Android-Image-Kitchen/tree/AIK-Linux > Add make vendor_boot > Adjustment and code optimization
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- tool_bin/boot_tools/AIK/cleanup.sh
+++ tool_bin/boot_tools/AIK/cleanup.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
-# AIK-mobile/cleanup: reset working directory
+# AIK-Linux/cleanup: reset working directory
 # osm0sis @ xda-developers
 
 case $1 in
-  --help) echo "usage: cleanup.sh [--quiet]"; return 1;
+  --help) echo "usage: cleanup.sh [--quiet]"; exit 1;
 esac;
 
-case $0 in
-  *.sh) aik="$0";;
-     *) aik="$(lsof -p $$ 2>/dev/null | grep -o '/.*cleanup.sh$')";;
+case $(uname -s) in
+  Darwin|Macintosh)
+    statarg="-f %Su";
+    readlink() { perl -MCwd -e 'print Cwd::abs_path shift' "$2"; }
+  ;;
+  *) statarg="-c %U";;
 esac;
-aik="$(dirname "$(readlink -f "$aik")")";
+
+aik=`cd "$( dirname $0 )" && pwd`;
 bin="$aik/bin";
 
-cd $aik;
-chmod -R 755 $bin $aik/*.sh;
-chmod 644 $bin/magic $bin/androidbootimg.magic $bin/BootSignature_Android.jar $bin/module.prop $bin/ramdisk.img $bin/avb/* $bin/chromeos/*;
+case $1 in
+  *) cd "$aik";;
+esac;
 
-rm -rf ramdisk split_img *new.* || return 1;
+chmod -R 755 "$bin" "$aik"/*.sh;
+chmod 644 "$bin/magic" "$bin/androidbootimg.magic" "$bin/boot_signer.jar" "$bin/avb/"* "$bin/chromeos/"*;
+
+if [ -d ramdisk ] && [ "$(stat $statarg ramdisk | head -n 1)" = "root" -o ! "$(find ramdisk 2>&1 | cpio -o >/dev/null 2>&1; echo $?)" -eq "0" ]; then
+  sudo=sudo;
+fi;
+
+$sudo rm -rf ramdisk split_img *new.* *ramdisk-*.* || exit 1;
 
 case $1 in
   --quiet) ;;
   *) echo "Working directory cleaned.";;
 esac;
-return 0;
 
