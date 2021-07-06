415380f1fc938f0df81e8039aeb533e3066654e3
xiaoxindada <2245062854@qq.com>
2021-07-07 02:34:00 +0800

    SGSI: Update Patchs copy.sh: Compress after each build Patch3: 1. Rebase with anykernel3 and compatible with dynamic partition devices 2. Fix permissive failed 3. Detect the current boot status when flashing 4. Disable privapp_permissions 5. Force apex not updatable 6. Read write mount partition is preferred 7. Add support for system_ext partition
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- copy.sh
+++ copy.sh
@@ -3,5 +3,15 @@
 LOCALDIR=`cd "$( dirname $0 )" && pwd`
 cd $LOCALDIR
 
+mkdir -p ./SGSI
 cp -frp ./other/* ./SGSI/
+for i in Patch{1..3};do
+  if [ -d ./SGSI/$i ];then
+    cd ./SGSI/$i
+    echo "正在打包$i中..."
+    zip -r ../$i.zip ./* 2&>/dev/null
+    cd $LOCALDIR
+    rm -rf ./SGSI/$i
+  fi
+done
 chmod 777 -R ./SGSI
