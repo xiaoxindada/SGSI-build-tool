a034cd7188a6cd24a89ed9a3035139c0b3695420
xiaoxindada <2245062854@qq.com>
2021-07-16 03:44:29 +0800

    apex_extractor.sh: Add capex support
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- make/apex_flat/apex_extractor.sh
+++ make/apex_flat/apex_extractor.sh
@@ -6,20 +6,33 @@ echo "ext4extract"
 EXT4EXTRACT="./ext4extract.py"
 
 APEXDIR="$1"
-APEXES=$(ls "$APEXDIR" | grep ".apex")
+APEXES=$(ls "$APEXDIR" | grep ".apex" | grep -v ".capex" )
 for APEX in $APEXES; do
-    #if echo "$APEX" | grep -q "google" ;then
-     #   APEXNAME=$(echo "$APEX" | sed 's/\.google//' | sed 's/.apex//')
-    #else
-        APEXNAME=$(echo "$APEX" | sed 's/.apex//')
-    #fi
+    APEXNAME=$(echo "$APEX" | sed 's/.apex//')
     if [[ -d "$APEXDIR/$APEXNAME" || -d "$APEXDIR/$APEX" ]] ; then
         continue
-    fi    
+    fi
     mkdir -p "$APEXDIR/$APEXNAME"
     7z e -y "$APEXDIR/$APEX" apex_payload.img apex_pubkey -o"$APEXDIR/$APEXNAME" >> "$TMPDIR"/zip.log
     $EXT4EXTRACT "$APEXDIR/$APEXNAME/apex_payload.img" -D "$APEXDIR/$APEXNAME" >> "$TMPDIR"/zip.log
     rm -rf "$APEXDIR/$APEXNAME/apex_payload.img"
     rm -rf "$APEXDIR/$APEXNAME/lost+found"
     #rm -rf "$APEXDIR/$APEX"
+done
+
+CAPEXES=$(ls "$APEXDIR" | grep ".capex" )
+for CAPEX in $CAPEXES; do
+   if [[ -z $CAPEX ]] ; then
+       continue
+   fi
+   CAPEXNAME=$(echo "$CAPEX" | sed 's/.capex//')
+   7z e -y "$APEXDIR/$CAPEX" original_apex -o"$APEXDIR" >> "$TMPDIR"/zip.log
+   rm -rf "$APEXDIR/$CAPEX"
+   mv -f "$APEXDIR/original_apex" "$APEXDIR/$CAPEXNAME.apex"
+   mkdir -p "$APEXDIR/$CAPEXNAME"
+   7z e -y "$APEXDIR/$CAPEXNAME.apex" apex_payload.img apex_pubkey -o"$APEXDIR/$CAPEXNAME" >> "$TMPDIR"/zip.log
+   $EXT4EXTRACT "$APEXDIR/$CAPEXNAME/apex_payload.img" -D "$APEXDIR/$CAPEXNAME" >> "$TMPDIR"/zip.log
+   rm -rf "$APEXDIR/$CAPEXNAME/apex_payload.img"
+   rm -rf "$APEXDIR/$CAPEXNAME/lost+found"
+   #rm -rf "$APEXDIR/$CAPEX"
 done
