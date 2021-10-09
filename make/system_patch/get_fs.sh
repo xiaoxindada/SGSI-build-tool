dfc80985fc24c5dbeeea89712cfa25c6147b0d97
xiaoxindada <2245062854@qq.com>
2021-10-09 18:51:07 +0800

    SGSI: Make all ROMs use prebuilt netd
    Signed-off-by: xiaoxindada <2245062854@qq.com>
    # based on https://github.com/erfanoabdi/ErfanGSIs/commit/daa17a4f4e8ae17656a51eba4825393748d51fda

--- make/system_patch/get_fs.sh
+++ make/system_patch/get_fs.sh
@@ -25,6 +25,9 @@ for files in $(find ./system/ -name "*");do
     if [ $(echo $files | grep ".sh$") ];then
       echo $files | sed "s#\./#/#g" | sed "s/^/&\/system/g" | sed "s/$/& u:object_r:update_engine_exec:s0/g" | sed 's|\.|\\.|g' >> $target_contexts
       echo $files | sed "s#\./#/#g" | sed "s/^/&system/g" | sed "s/$/& 0 2000 0755/g" >> $target_fs
+    elif [ $(echo $files | grep "/bin/") ];then
+      echo $files | sed "s#\./#/#g" | sed "s/^/&\/system/g" | sed "s/$/& u:object_r:u:object_r:system_file:s0/g" | sed 's|\.|\\.|g' >> $target_contexts
+      echo $files | sed "s#\./#/#g" | sed "s/^/&system/g" | sed "s/$/& 0 2000 0755/g" >> $target_fs    
     else
       echo $files | sed "s#\./#/#g" | sed "s/^/&\/system/g" | sed "s/$/& u:object_r:system_file:s0/g" | sed 's|\.|\\.|g' >> $target_contexts
       echo $files | sed "s#\./#/#g" | sed "s/^/&system/g" | sed "s/$/& 0 0 0644/g" >> $target_fs
@@ -34,3 +37,7 @@ done
 
 sed -i '1d' $target_contexts
 sed -i '1d' $target_fs
+
+if [ -f $LOCALDIR/remove_define.sh ];then
+  $LOCALDIR/remove_define.sh
+fi
