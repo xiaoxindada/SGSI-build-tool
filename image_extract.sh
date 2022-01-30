1343880bb9d50cd15cdbe01e0fe2b5a5867b359f
xiaoxindada <2245062854@qq.com>
2022-01-30 18:19:46 +0800

    tool: File system type check fixed for image
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- image_extract.sh
+++ image_extract.sh
@@ -4,25 +4,28 @@ LOCALDIR=`cd "$( dirname ${BASH_SOURCE[0]} )" && pwd`
 cd $LOCALDIR
 source ./bin.sh
 source ./language_helper.sh
+EROFS_MAGIC_V1="e2e1f5e0" # 0xE0F5E1E2
+EXT_MAGIC="53ef" # 0xEF53
+EXT_OFFEST="1080"
+EROFS_OFFEST="1024"
+partitions="system vendor product system_ext"
 
-partition_name="
-system
-vendor
-product
-system_ext
-"
 rm -rf $TARGETDIR
 mkdir -p $TARGETDIR
 
-for partition in $partition_name ;do
-  if [[ -e $IMAGESDIR/$partition.img ]];then
-    echo "$EXTRACTING_STR $partition.img..."
-    if ! python3 $bin/imgextractor.py $IMAGESDIR/$partition.img $TARGETDIR ;then
-      echo -e "\033[33m $EXTRACTING_STR $EROFS_STR $partition.img... \033[0m"
-      if ! $bin/erofsUnpackKt $IMAGESDIR/$partition.img $TARGETDIR ;then
-        echo "$FAILEXTRACT_STR $partition.img"
-        exit 1
-      fi  
+for partition in $partitions ;do
+  if [[ -e $IMAGESDIR/${partition}.img ]];then
+    if [ $(xxd -p -l "2" --skip "$EXT_OFFEST" "$IMAGESDIR/${partition}.img") = "$EXT_MAGIC" ];then
+      echo "$DETECTED_EXT_FILESYSTEM_IMAGE"
+      echo "${partition}.img $EXTRACTING_STR"
+      python3 $bin/imgextractor.py "$IMAGESDIR/${partition}.img" "$TARGETDIR"
+      [ $? != 0 ] && echo "${partition}.img $FAILEXTRACT_STR" && exit 1
+    elif [ $(xxd -p -l "4" --skip "$EROFS_OFFEST" "$IMAGESDIR/${partition}.img") = "$EROFS_MAGIC_V1" ];then
+      echo "$DETECTED_EROFS_FILESYSTEM_IMAGE"
+      echo "${partition}.img $EXTRACTING_STR"
+      $bin/erofsUnpackKt "$IMAGESDIR/${partition}.img" "$TARGETDIR"
+      [ $? != 0 ] && echo "${partition}.img $FAILEXTRACT_STR" && exit 1
     fi
+    continue
   fi
 done
--- unpackimg.sh
+++ unpackimg.sh
@@ -6,17 +6,24 @@ LOCALDIR=`cd "$( dirname ${BASH_SOURCE[0]} )" && pwd`
 cd $LOCALDIR
 source ./bin.sh
 
-rm -rf ./out
-mkdir ./out
+EROFS_MAGIC_V1="e2e1f5e0" # 0xE0F5E1E2
+EXT_MAGIC="53ef" # 0xEF53
+EXT_OFFEST="1080"
+EROFS_OFFEST="1024"
+
+rm -rf $LOCALDIR/out
+mkdir $LOCALDIR/out
 
 echo ""
 read -p "请输入要接包的分区(别带.img): " species
-
-echo "正在解压$species..."
-if ! python3 $bin/imgextractor.py ./$species'.img' ./out ;then
-  echo "正在尝试使用erofs解压"
-  if ! $bin/erofsUnpackKt $species'.img' ./out 2&>/dev/null ;then
-    echo "$species'.img 解压失败！"
-  fi
+if [ $(xxd -p -l "2" --skip "$EXT_OFFEST" "${species}.img") = "$EXT_MAGIC" ];then
+  echo "检测到 ${species}.img 为 ext2/3/4 文件系统"
+  echo "正在解压${species}.img..."
+  python3 $bin/imgextractor.py ${species}.img $LOCALDIR/out
+  [ $? != 0 ] && echo "解压 ${species}.img 失败" && exit 1
+elif [ $(xxd -p -l "4" --skip "$EROFS_OFFEST" "${species}.img") = "$EROFS_MAGIC_V1" ];then
+  echo "检测到 ${species}.img 为 erofs 文件系统"
+  echo "正在解压${species}.img..."
+  $bin/erofsUnpackKt ${species}.img $LOCALDIR/out 
+  [ $? != 0 ] && echo "解压 ${species}.img 失败" && exit 1
 fi
-
