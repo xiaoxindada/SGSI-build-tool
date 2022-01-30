141d36859fd22e9fc2f75459c94dce6fb9f9a8c6
xiaoxindada <2245062854@qq.com>
2022-01-31 04:47:50 +0800

    tool: Change URL2SGSI
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- url2SGSI.sh
+++ url2SGSI.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Inspired from url2GSI from ErfanGSIs tool at https://github.com/erfanoabdi/ErfanGSIs
-# Copyright to Rahul at https://github.com/rahulkhatri137
+# Rebased: Rahul at https://github.com/rahulkhatri137
+# Final change: Xiaoxindada at https://github.com/xiaoxindada
 
 LOCALDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source ./bin.sh
-source ./language_helper.sh
 fixbug=true
 build="--ab"
 
@@ -15,83 +15,85 @@ Usage:
 $0 <Firmware link> <Firmware type> <Build Type> <Fixbug>
    Firmware link: Firmware download link or local path
    Firmware type: Firmware source type
-   Build Type: [--ab|-ab|--AB|-AB] or [--a|-a|-A|--A]
-   Fixbug: [--fb|-fb] to disable Fixbug
+   Build Type: ab or a
+   Fixbug: [fb|--fb|-fb] to disable Fixbug
 EOT
+exit 1
 }
 
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
+if [ $# -lt 2 ];then
+  usage
+fi
 
-case $key in
+case "$1" in
     --help|-h|-?)
     usage
-    exit 1
-    ;;
-    --fb|-fb)
-    fixbug=false
-    shift
-    ;;
-    --ab|-ab|--AB|-AB)
-    build="-ab"
-    shift
-    ;;
-    --a|-a|-A|--A)
-    build="-a"
-    shift
-    ;;
-    *)
-    POSITIONAL+=("$1")
-    shift
     ;;
 esac
-done
-set -- "${POSITIONAL[@]}" # restore positional parameters
-
-if [[ ! -n $2 ]]; then
-    echo "-> ERROR!"
-    echo " - Enter all needed parameters"
-    usage
-    exit 1
-fi
 
 URL=$1
 shift
+
 TYPE=$1
 shift
 
-ORIGINAL_URL=$URL
-
 if ! (cat $LOCALDIR/make/rom_support_list.txt | grep -qo "$TYPE");then
-  echo $UNSUPPORTED_ROM
-  echo $SUPPORTED_ROM_LIST
+  echo "Current rom type not support"
+  echo "List of supported:"
   cat $LOCALDIR/make/rom_support_list.txt
   exit 1
 fi
 
-rm -rf tmp output workspace SGSI
+build="$1"
+case $build in
+    ab|AB|--ab|-ab|--AB|-AB)
+    build="--ab"
+    shift
+    ;;
+    a|A|--a|-a|-A|--A)
+    build="-a"
+    shift
+    ;;
+    *)
+    build="--ab"
+    ;;
+esac
 
-URL="$1"
-if [[ "$URL" == "http"* ]]; then
-echo "Downloading firmware..."
-mkdir -p "$LOCALDIR/tmp"
-ZIP="$LOCALDIR/tmp/update.zip"
-ZIP_NAME="update.zip"
-aria2c -x16 -j$(nproc) -U "Mozilla/5.0" -d "$LOCALDIR/tmp" -o "$ZIP_NAME" ${URL} > /dev/null 2>&1
-URL="$ZIP"
-fi
+fixbug="$1"
+case $fixbug in
+    fb|--fb|--FB|-fb|-FB)
+    fixbug=false
+    shift
+    ;;
+    *)
+    fixbug=true
+    shift
+    ;;
+esac
+
+rm -rf tmp output workspace SGSI
 
 LEAVE() {
     echo "SGSI failed! Exiting..."
     rm -rf "$LOCALDIR/output" "$LOCALDIR/workspace" "$LOCALDIR/tmp" "$LOCALDIR/SGSI"
     exit 1
 }
+
+if [[ "$URL" == "http"* ]]; then
+  echo "Downloading firmware..."
+  mkdir -p "$LOCALDIR/tmp"
+  ZIP="$LOCALDIR/tmp/update.zip"
+  ZIP_NAME="update.zip"
+  aria2c -x16 -j$(nproc) -U "Mozilla/5.0" -d "$LOCALDIR/tmp" -o "$ZIP_NAME" ${URL}
+else
+  echo "Url not support!"
+  exit 1
+fi
+
 if [ $fixbug == true ]; then
-    "$LOCALDIR"/make.sh $build $TYPE $URL --fix-bug || LEAVE
+  "$LOCALDIR"/make.sh $build $TYPE $ZIP --fix-bug || LEAVE
 elif [ $fixbug == false ] ; then
-    "$LOCALDIR"/make.sh $build $TYPE $URL || LEAVE
+  "$LOCALDIR"/make.sh $build $TYPE $ZIP || LEAVE
 fi
 
 rm -rf "$LOCALDIR/tmp"
@@ -99,5 +101,5 @@ rm -rf "$LOCALDIR/workspace"
 if [ -d "$LOCALDIR/SGSI" ]; then
    echo "Porting SGSI done!"
 else
-   LEAVE
+  LEAVE
 fi
