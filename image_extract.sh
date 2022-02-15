abeaf3b7895dea56cf03557ef6b662cacfe09088
xiaoxindada <2245062854@qq.com>
2022-02-15 19:34:38 +0800

    tool: unpackimg.sh: Fix sparse.img extract and typo
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- image_extract.sh
+++ image_extract.sh
@@ -6,8 +6,8 @@ source ./bin.sh
 source ./language_helper.sh
 EROFS_MAGIC_V1="e2e1f5e0" # 0xE0F5E1E2
 EXT_MAGIC="53ef" # 0xEF53
-EXT_OFFEST="1080"
-EROFS_OFFEST="1024"
+EXT_OFFSET="1080"
+EROFS_OFFSET="1024"
 partitions="system vendor product system_ext"
 
 rm -rf $TARGETDIR
@@ -15,12 +15,12 @@ mkdir -p $TARGETDIR
 
 for partition in $partitions ;do
   if [[ -e $IMAGESDIR/${partition}.img ]];then
-    if [ $(xxd -p -l "2" --skip "$EXT_OFFEST" "$IMAGESDIR/${partition}.img") = "$EXT_MAGIC" ];then
+    if [ $(xxd -p -l "2" --skip "$EXT_OFFSET" "$IMAGESDIR/${partition}.img") = "$EXT_MAGIC" ];then
       echo "$DETECTED_EXT_FILESYSTEM_IMAGE"
       echo "${partition}.img $EXTRACTING_STR"
       python3 $bin/imgextractor.py "$IMAGESDIR/${partition}.img" "$TARGETDIR"
       [ $? != 0 ] && echo "${partition}.img $FAILEXTRACT_STR" && exit 1
-    elif [ $(xxd -p -l "4" --skip "$EROFS_OFFEST" "$IMAGESDIR/${partition}.img") = "$EROFS_MAGIC_V1" ];then
+    elif [ $(xxd -p -l "4" --skip "$EROFS_OFFSET" "$IMAGESDIR/${partition}.img") = "$EROFS_MAGIC_V1" ];then
       echo "$DETECTED_EROFS_FILESYSTEM_IMAGE"
       echo "${partition}.img $EXTRACTING_STR"
       $bin/erofsUnpackKt "$IMAGESDIR/${partition}.img" "$TARGETDIR"
