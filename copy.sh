781bf5ca81960b1c7b8338fc42cabea3dd5bf654
xiaoxindada <2245062854@qq.com>
2022-02-17 01:00:32 +0800

    SGSI: patch: Add Vbmeta_Patch
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- copy.sh
+++ copy.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-LOCALDIR=`cd "$( dirname ${BASH_SOURCE[0]} )" && pwd`
+LOCALDIR=$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)
 cd $LOCALDIR
+source ./bin.sh
 source ./language_helper.sh
 
 mkdir -p ./SGSI
 cp -frp ./other/* ./SGSI/
-for i in Patch{1..3};do
-  if [ -d ./SGSI/$i ];then
+for i in Patch{1..3}; do
+  if [ -d ./SGSI/$i ]; then
     cd ./SGSI/$i
     echo "$GENERATRING_STR $i"
-    zip -r ../$i.zip ./* 2&>/dev/null
+    zip -r ../$i.zip ./* 2 &>/dev/null
     cd $LOCALDIR
     rm -rf ./SGSI/$i
   fi
 done
+
+echo "$GENERATRING_STR Vbmeta_Patch"
+#$bin/apex_tools/avbtool.py make_vbmeta_image --set_hashtree_disabled_flag --flags 2 --padding_size 4096 --output ./SGSI/Vbmeta_Patch/vbmeta_disabled.img
+cd ./SGSI/Vbmeta_Patch
+zip -r ../Vbmeta_Patch.zip ./* 2 &>/dev/null
+cd $LOCALDIR
+rm -rf ./SGSI/Vbmeta_Patch
 chmod 777 -R ./SGSI
