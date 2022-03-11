1c78b2304d78bbb78fc0ec4b71c6678165be2ca2
xiaoxindada <2245062854@qq.com>
2022-03-12 06:05:59 +0800

    tool: Add ectract squashfs filesystem image support
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- unpackimg.sh
+++ unpackimg.sh
@@ -9,9 +9,11 @@ source ./bin.sh
 EROFS_MAGIC_V1="e2e1f5e0" # 0xE0F5E1E2
 EXT_MAGIC="53ef" # 0xEF53
 SPARSE_MAGIC="3aff26ed" # 0xed26ff3a
+SQUASHFS_MAGIC="68737173" # 0x73717368
 EXT_OFFSET="1080"
 EROFS_OFFSET="1024"
 SPARSE_OFFSET="0"
+SQUASHFS_OFFSET="0"
 
 rm -rf $LOCALDIR/out
 mkdir $LOCALDIR/out
@@ -35,6 +37,10 @@ elif [ $(xxd -p -l "4" --skip "$EROFS_OFFSET" "${species}.img") = "$EROFS_MAGIC_
   echo "检测到 ${species}.img 为 erofs 文件系统"
   echo "正在解压${species}.img..."
   $bin/erofsUnpackKt ${species}.img $LOCALDIR/out 
+  [ $? != 0 ] && echo "解压 ${species}.img 失败" && exit 1
+elif [ $(xxd -p -l "4" --skip "$SQUASHFS_OFFSET" "${species}.img") = "$SQUASHFS_MAGIC" ];then
+  rm -rf $LOCALDIR/out/${species}
+  unsquashfs -q -n -u -d $LOCALDIR/out/${species} ${species}.img
   [ $? != 0 ] && echo "解压 ${species}.img 失败" && exit 1
 else
   echo "当前img不支持解压 请检查img的文件系统"
