5171769597234cb21ec3165370a65966acc6d9e0
PdyLZY-233 <2900357764@qq.com>
2022-08-22 22:42:50 +0800

    增加更多可精简列表
    Signed-off-by: PdyLZY-233 <2900357764@qq.com>

--- apps_clean/flyme.sh
+++ apps_clean/flyme.sh
@@ -5,7 +5,14 @@ cd $LOCALDIR
 
 systemdir=$1
 
+rm -rf $1/MzApp/*
+rm -rf $1/product/custom/*
+
 apps_dir="
+AlwaysOnDisplay
+FlymeAlarmClock
+MzUpdate
+WPS_Meizu_Version
 DataMigration
 Email
 Life
@@ -17,6 +24,17 @@ DesktopBackup
 FamilyGuard
 GmsCore
 Phonesky_CN
+Browser
+DirectService
+EasyLauncher
+FlymeGallery
+FlymeLab
+FlymeMusic
+FlymeSoundRecorder
+MeizuPay
+PhotoService
+Search
+Video
 "
 for delete_dir in $apps_dir ;do
   find $systemdir -type d -name "$delete_dir" | xargs rm -rf
